c4f892cb1057774ac713fec099ffa2d5e385045e
rajranerr <raneraj71@gmail.com>
2026-05-07 16:54:33 +0530

    add new codes of python program

--- prectice.py
+++ prectice.py
@@ -1,1 +1,35 @@
 print("hello world")
+
+# Basic Arithmetic:
+
+a,b=4,6
+print(f"addition: {a+b}")
+print(f"division: {a/b}")
+
+# Vaariable swepping:
+
+x,y=7,4
+x,y=y,x
+print(x,y)
+
+# If-Else Condition:
+
+num=8
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")    
+
+# Loops:
+
+for i in range(4):
+    print(i)    
+
+# defining a function:
+
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("tilak"))
+
+
